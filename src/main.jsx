bc14f00ae6f3a93039b69e774f41607bc359e3ae
Luisrds <luisaugusto.social@gmail.com>
2026-09-03 10:18:03 -0300

    feat: adicionar acesso às fotos no painel admin

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import Admin from './Admin'
+import AdminFotos from './AdminFotos'
 import './styles.css'
 import './liquid-glass.css'
 
-const admin = window.location.pathname.startsWith('/admin')
+const path = window.location.pathname
+const photoAdmin = path.startsWith('/admin/fotos')
+const admin = path.startsWith('/admin')
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>{admin ? <Admin /> : <App />}</React.StrictMode>
+  <React.StrictMode>
+    {photoAdmin ? <AdminFotos /> : admin ? <><Admin /><a className="admin-photo-shortcut" href="/admin/fotos">Alterar fotos da Brenda</a></> : <App />}
+  </React.StrictMode>
 )
